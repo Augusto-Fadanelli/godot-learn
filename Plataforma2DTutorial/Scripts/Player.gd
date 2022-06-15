3e99bb48e2def7a6c23c058d5a721f3644ac44b2
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-06-15 14:07:05 -0300

    Tem algum erro no _set_animation

--- Plataforma2DTutorial/Scripts/Player.gd
+++ Plataforma2DTutorial/Scripts/Player.gd
@@ -1,22 +1,54 @@
 extends KinematicBody2D
 
-var velocity = Vector2() # Vetor vazio
+var velocity = Vector2.ZERO
 var move_speed = 480
 var gravity = 1200
-var jump_force = -720
+var jump_force = -500
+var is_grounded # Verifica se está no chão
+onready var raycasts = $raycasts
 
-func _physics_process(delta):
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	#print(move_direction)
-	
+func _physics_process(delta):	
 	velocity.y += move_speed * delta
-	#print(velocity.y)
-	velocity.x = move_speed * move_direction
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = jump_force /3
+	_get_input()
+
+	velocity = move_and_slide(velocity)
+	
+	is_grounded = _check_is_grounded()
+	
+	_set_animation()
+
+func _get_input():
+	velocity.x = 0
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+	
+	if move_direction != 0:
+		$texture.scale.x = move_direction
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("jump") and is_grounded:
+		velocity.y = jump_force / 2
+
+func _check_is_grounded():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
+
+func _set_animation():
+	var anim = "idle"
+	
+	#if !is_grounded:
+	#	anim = "jump"
+	if velocity.x != 0:
+		anim = "run"
+	else:
+		anim = "idle"
 		
-	move_and_slide(velocity)
+	if $anim.assigned_animation != anim:
+		print("teste")
+		$anim.play(anim)
 
 func _ready():
 	pass
